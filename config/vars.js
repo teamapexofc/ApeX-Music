a762289d5509a1c150f325dad3022b837f239a2d
ApeX Development <contact.apexdevs@gmail.com>
2026-03-27 13:51:44 +0530

    Update vars.js

--- config/vars.js
+++ config/vars.js
@@ -1,22 +1,22 @@
 const vars = {
     token: "",
     prefix: "!",
-    mongoURL: "", 
-    owners: [""],
+    mongoURL: "mongodb+srv://apexdevs:apexdevss@apexdevs.ujdutzg.mongodb.net/?appName=Apexdevs", 
+    owners: ["1304080189029875753"],
     color: "#ff0000" ,// embed color
     webhooks: {
         cmd: ""
     },
     nodes: [{
         name: "ApeX",
-        url: "lava-v4.ajieblogs.eu.org:443",
-        auth: "https://dsc.gg/ajidevserver",
-        secure: true
+        url: "lava.mimibot.app:5017",
+        auth: "AliveMimi",
+        secure: false
     }],
-    serverLink: "",
-    supportId: "",
-    inviteLink: "",
-    setupImg: null
+    serverLink: "https://discord.gg/mw6KwT5EGz",
+    supportId: "1486969202760880140",
+    inviteLink: "https://discord.com/oauth2/authorize?client_id=1482264720357457980&scope=bot%20applications.commands&permissions=8",
+    setupImg: "https://media.discordapp.net/attachments/1444571950268551332/1486972644048900227/ChatGPT_Image_Mar_27_2026_01_59_37_AM.png?ex=69c772b0&is=69c62130&hm=35c1ecd0112805c3f8d287f1e13cbed92e76c21ad2c74126d46da7496c296ca1&=&format=webp&quality=lossless&width=1331&height=887"
 }
 
 module.exports = vars;
